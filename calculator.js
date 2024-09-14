878a7e6f4931c878e79df377748004d5dc14d7f3
doudoumdh <141367756+doudoumdh@users.noreply.github.com>
2024-09-14 01:37:20 -0400

    Simple update

--- calculator.js
+++ calculator.js
@@ -20,7 +20,7 @@ function operation() {
     }
 
     // Append a new row to the existing table
-    document.getElementById("resultTable").innerHTML += 
+    document.getElementById("resultTable").innerHTML +=
         "<tr><td>" + x + "</td><td>" + op + "</td><td>" + y + "</td><td>" + result + "</td></tr>";
 }
 
@@ -28,13 +28,15 @@ let ans = prompt("Do you wanna use the Calculator? (Y/N)");
 
 if (ans === "Y" || ans === "y") {
     // Create the table structure once at the beginning if the user chooses to use the calculator 
-    
+
     document.write("<table border='1' id='resultTable'>");
     document.write("<tr style='background-color: orange;'><th>X</th><th>Op</th><th>Y</th><th>Result</th></tr>");
 
-    
+
     while (ans === "Y") {
         operation();
+
+
         ans = prompt("Do you wanna use the Calculator again? (Y/N)");
     }
 
