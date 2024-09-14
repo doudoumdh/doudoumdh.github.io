6a92e2a63b8f50a797b4687ae599fea8395cb8d5
doudoumdh <141367756+doudoumdh@users.noreply.github.com>
2024-09-14 01:15:54 -0400

    Calculator Added

---
+++ calculator.js
@@ -0,0 +1,44 @@
+function operation() {
+    let x = parseFloat(prompt("Enter your first value"));
+    let op = prompt("Enter your operator (+, -, *, /, %)");
+    let y = parseFloat(prompt("Enter your second value"));
+
+    let result;
+
+    if (op === "+") {
+        result = x + y;
+    } else if (op === "-") {
+        result = x - y;
+    } else if (op === "*") {
+        result = x * y;
+    } else if (op === "/") {
+        result = x / y;
+    } else if (op === "%") {
+        result = x % y;
+    } else {
+        result = "Invalid operator";
+    }
+
+    // Append a new row to the existing table
+    document.getElementById("resultTable").innerHTML += 
+        "<tr><td>" + x + "</td><td>" + op + "</td><td>" + y + "</td><td>" + result + "</td></tr>";
+}
+
+let ans = prompt("Do you wanna use the Calculator? (Y/N)");
+
+if (ans === "Y" || ans === "y") {
+    // Create the table structure once at the beginning
+    
+    document.write("<table border='1' id='resultTable'>");
+    document.write("<tr style='background-color: orange;'><th>X</th><th>Op</th><th>Y</th><th>Result</th></tr>");
+
+    
+    while (ans === "Y") {
+        operation();
+        ans = prompt("Do you wanna use the Calculator again? (Y/N)");
+    }
+
+    document.write("</table>");
+} else {
+    document.write("Exiting...");
+}
