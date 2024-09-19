c0abf75f60054f57474972dcb01d231f61812355
doudoumdh <141367756+doudoumdh@users.noreply.github.com>
2024-09-19 09:38:15 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,11 @@
-// Global result list to store all results
+//an array list to hold all results for future use
 let resultList = [];
 
+
+//function to perform operation based on operation sign
 function operation() {
     let x = parseFloat(prompt("Enter your first value"));
-    let op = prompt("Enter your operator (+, -, *, /, %)"); 
+    let op = prompt("Enter your operator (+, -, *, /, %)");
     let y = parseFloat(prompt("Enter your second value"));
 
     let result;
@@ -15,7 +17,7 @@ function operation() {
         result = x - y;
     } else if (op === "*") {
         result = x * y;
-    } else if (op === "/" && y!=0) {
+    } else if (op === "/") {
         result = x / y;
     } else if (op === "%") {
         result = x % y;
@@ -40,19 +42,21 @@ function minMaxAvg() {
     let avg;
     let total;
     if (resultList.length === 0) return { min: 0, max: 0, avg: 0, total: 0 };
-    
+
     min = Math.min(...resultList);
     max = Math.max(...resultList);
-    
-   
+
+
     total = resultList.reduce((a, b) => a + b, 0);
     avg = total / resultList.length;
 
+    //we return the new values assigned
     return { min, max, avg, total };
 }
 
 let ans = prompt("Do you wanna use the Calculator? (Y/N)");
 
+//prompt the user to continue or not
 if (ans === "Y" || ans === "y") {
     // Create the table structure once at the beginning if the user chooses to use the calculator
     document.write("<table border='1' id='resultTable'>");
@@ -65,12 +69,12 @@ if (ans === "Y" || ans === "y") {
 
     // After exiting the loop, calculate and display the min, max, avg, and total
     let stats = minMaxAvg();
-    
+
     document.write("<table border='1' id='minMaxtable'>");
     document.write("<tr style='background-color: orange;'><th>Min</th><th>Max</th><th>Avg</th><th>Total</th></tr>");
     document.write("<tr><td>" + stats.min + "</td><td>" + stats.max + "</td><td>" + stats.avg.toFixed(2) + "</td><td>" + stats.total + "</td></tr>");
     document.write("</table>");
-    
+
 } else {
     document.write("Exiting...");
 }
